5b60d941a2a0dbaeb2be9f687c6f5e8a2941a5f9
Tao Jinglin <694668175@qq.com>
2018-05-15 11:03:03 -0700

    update code

--- python/bus_modi.py
+++ python/bus_modi.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 17 19:21:30 2018
+Created on Tue May 15 10:47:30 2018
 
-@author: lusha
+@author: Jinglin
 """
 
 import csv
 import igraph
 import numpy
-import matplotlib.pyplot as plt
-#import louvain
 import modification
-numpy.set_printoptions(threshold=numpy.inf) 
 import time
+import sys
 
 graph9bus=igraph.Graph() 
     
-with open('39busnode.csv','rb') as csvfileNode:
+with open(str(sys.argv[1]) + 'busnode.csv','rb') as csvfileNode:
     csvreaderNode=csv.reader(csvfileNode)
     mycsvNode=list(csvreaderNode)
     for row in mycsvNode:
@@ -26,7 +24,7 @@ nodeNumber=graph9bus.vcount()
 
 Bmatrix=numpy.zeros((nodeNumber,nodeNumber))
 SVQ=numpy.zeros((nodeNumber,nodeNumber))
-with open('39busbranch.csv','rb') as csvfileBranch:
+with open(str(sys.argv[1]) + 'busbranch.csv','rb') as csvfileBranch:
     csvreaderBranch=csv.reader(csvfileBranch)
     mycsvBranch=list(csvreaderBranch)
     for row in mycsvBranch:
@@ -45,7 +43,7 @@ for i in range(nodeNumber):
 SVQ=numpy.linalg.pinv(Bmatrix)
 #print SVQ
 
-with open('39busQ.csv','rb') as csvfileQ:
+with open(str(sys.argv[1]) + 'busQ.csv','rb') as csvfileQ:
     csvreaderQ=csv.reader(csvfileQ)
     mycsvQ=list(csvreaderQ)
     for row in mycsvQ:
@@ -53,36 +51,15 @@ with open('39busQ.csv','rb') as csvfileQ:
         graph9bus.vs.select(int(row[0])-1)["Qsupply"]=float(row[1])
         graph9bus.vs.select(int(row[0])-1)["Qdemand"]=float(row[2])
 
-
-
-'''
-# using Betweeness
-vd=graph9bus.community_edge_betweenness(clusters=None, directed=True, weights=None)
-mod=graph9bus.modularity(vd.as_clustering())
-print 'Using Betweeness:',vd.as_clustering()
-print mod
-
-# using Label propagation
-clusters=graph9bus.community_label_propagation(weights=None, initial=None, fixed=None)
-mod=graph9bus.modularity(clusters)
-print 'Using Label propagation',clusters
-print mod
-'''
 # using Louvain
 start_time = time.time()
 clusters=graph9bus.community_multilevel()
 mod=graph9bus.modularity(clusters)
-#print 'Using Lovain:',clusters
-print 'mod:',mod
 
 #louvain.louvain(graph9bus,SVQ)
 newmod=modification.modification(mod,graph9bus,SVQ,clusters)
 
-#print 'new mod',newmod
-#print graph9bus
-#f = open('data.txt', 'w')
-#print >> f, SVQ
-#f.close()
 end_time = time.time()
-print (end_time - start_time)
-print 'new mod',newmod
+print 'Degree distribution',graph9bus.degree_distribution()
+print 'Running time:',(end_time - start_time)
+print 'New mod',newmod
